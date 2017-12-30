2a8a5e8a5be02d831c15490bea56a494ce2b19ba
Jose Cervantes <abdn0518@gmail.com>
2017-12-30 17:34:17 -0600

    Part 1 of MerryFragmas Complete
    Has everything the first video has.

---
+++ Assets/Scripts/ShotEffectsManager.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class ShotEffectsManager : MonoBehaviour
+{
+    [SerializeField] ParticleSystem muzzleFlash;
+    [SerializeField] AudioSource gunAudio;
+    [SerializeField] GameObject impactPrefab;
+
+    ParticleSystem impactEffect;
+
+    //Create the impact effect for our shots
+    public void Initialize()
+    {
+        impactEffect = Instantiate(impactPrefab).GetComponent<ParticleSystem>();
+    }
+
+    //Play muzzle flash and audio
+    public void PlayShotEffects()
+    {
+        muzzleFlash.Stop(true);
+        muzzleFlash.Play(true);
+        gunAudio.Stop();
+        gunAudio.Play();
+    }
+
+    //Play impact effect and target position
+    public void PlayImpactEffect(Vector3 impactPosition)
+    {
+        if (impactEffect != null)
+        {
+            impactEffect.transform.position = impactPosition;
+            impactEffect.Stop();
+            impactEffect.Play();
+        }
+    }
+}
